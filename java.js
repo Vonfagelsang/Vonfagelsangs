16d43d4064525843572b00e916cc72fa297b7c05
Vonfagelsang <89455450+Vonfagelsang@users.noreply.github.com>
2022-04-08 14:20:40 +0200

    Add files via upload

---
+++ java.js
@@ -0,0 +1,9 @@
+function Meny() {
+    var x = document.getElementById("länkarna");
+    if (x.style.display === "block"){
+        x.style.display = "none";
+    }
+    else{
+        x.style.display = "block";
+    }
+}
