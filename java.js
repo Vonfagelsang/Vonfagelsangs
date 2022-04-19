cbe05401557df349386801676304e91ad3651440
Vonfagelsang <89455450+Vonfagelsang@users.noreply.github.com>
2022-04-19 14:12:24 +0200

    Add files via upload

--- java.js
+++ java.js
@@ -1,9 +1,3 @@
-function Meny() {
-    var x = document.getElementById("länkarna");
-    if (x.style.display === "block"){
-        x.style.display = "none";
-    }
-    else{
-        x.style.display = "block";
-    }
+function toggleMobileMenu(menu) {
+    menu.classList.toggle('open');
 }
